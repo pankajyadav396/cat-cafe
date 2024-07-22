305b5b4bbede894bed3e0c917288c478470db158
Your Name <you@example.com>
2024-07-22 10:25:09 +0530

    update code quality

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,3 +16,39 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// ============================   FORMVALIDATION   ============================
+function validateForm(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const nameError = document.getElementById("name-error");
+    const emailError = document.getElementById("email-error");
+
+    nameError.textContent = "";
+    emailError.textContent = "";
+
+    let isValid = true;
+
+    if (name === "" || /\d/.test(name)) {
+        nameError.textContent = "Please enter your name properly.";
+        isValid = false;
+    }
+
+    if (email === "" || !email.includes("@")) {
+        emailError.textContent = "Please enter a valid email address.";
+        isValid = false;
+    }
+    // Prevent form from submitting and refreshing the page
+    if (validateForm()) {
+        // Clear the input fields
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+
+        // Optionally, show a success message
+        alert("Form submitted successfully!");
+        console.log("name")
+        console.log("email")
+    }
+    return isValid;
+}
